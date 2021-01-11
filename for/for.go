f50a103a4e830a97cfc4f6a96da5c7e0c8359061
mahuihuang <mahuihuang@ghostcloud.cn>
2021-01-11 22:29:57 +0800

    feat(day1): init

---
+++ for/for.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+	// 条件为 true 的 while 循环
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
---
+++ if-else/if-else.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
+
+// Go 没有三目运算符,即使是基本的条件判断,依然需要使用完整的 if 语句
