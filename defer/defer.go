70bf7a9354849fcb9a05d1e73010f4e2ea32a47b
Ma Huihuang <007mhh@gmail.com>
2025-10-24 18:06:13 +0800

    feat: 完成 11 个用例抄录

---
+++ defer/defer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f := CreateFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func CreateFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+
+	if err != nil {
+		panic(err)
+	}
+}
